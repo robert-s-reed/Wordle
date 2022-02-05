3a8d8b5beb882120b6309f08f81457fef0ad5375
Robert Reed <robert-s-reed@outlook.com>
2022-02-05 16:43:08 +0000

    Added keyboard

--- index.js
+++ index.js
@@ -5,23 +5,24 @@ const absentCol = "Gray"
 const guesses = document.getElementsByClassName("guess-row") // Row in table containing guess letters
 var letterCells // Cells in table containing a letter of the guess
 var solution = "hello"
+const correctLetters = []
 var guessIndex = 0
 
 window.onkeyup = keyUp
 
 function keyUp(e)
 {
-    const input = e.target.value
+    const input = e.target.value.toLowerCase()
 
     if (input == undefined)
         return
     
-    letterCells = guesses[guessIndex].getElementsByClassName("letter") // Assign letterCells to the cells of the current guess
+    letterCells = guesses[guessIndex].getElementsByClassName("guess-letter") // Assign letterCells to the cells of the current guess
 
     for (var i = 0; i < 5; i++) // For each letter cell
     {
         if (i < input.length)
-            letterCells[i].innerHTML = input.charAt(i) // Set cell value to the corresponding letter in the guess
+            letterCells[i].innerHTML = input.charAt(i).toUpperCase() // Set cell value to the corresponding letter in the guess
         else
             letterCells[i].innerHTML = "" // Ensure cell is empty (e.g., if a character was removed)
     }
@@ -37,9 +38,19 @@ function checkGuess(guess)
         const guessChar = guess.charAt(i)
 
         if (guessChar == solution.charAt(i)) // Is the letter in the same position in the solution?
+        {
             letterCells[i].style.backgroundColor = correctCol
+            document.getElementById(guessChar).style.backgroundColor = correctCol
+            correctLetters.push(guessChar)
+        }
         else if (solution.includes(guessChar)) // Does the letter appear in any position in the solution?
+        {
             letterCells[i].style.backgroundColor = presentCol
+            
+            const kbLetter = document.getElementById(guessChar)
+            if (!correctLetters.includes(guessChar))
+                kbLetter.style.backgroundColor = presentCol
+        }
         else // Letter absent in solution
             letterCells[i].style.backgroundColor = absentCol
     }
